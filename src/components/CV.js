e92146aeed2c42783ceb0c7e508488eea83bd8a2
Catalina Campos <catalina.camposcaceres9@gmail.com>
2020-03-04 11:47:53 -0300

    about me and cv modal

--- src/components/CV.js
+++ src/components/CV.js
@@ -1,15 +1,26 @@
 import React, { Component } from "react";
-import { Container, Row, Col, Image } from "react-bootstrap";
-// import uno from "../assets/Project1/1.jpg";
-import "./Project1.css";
+import { Container, Row, Col, Image, Button } from "react-bootstrap";
+import cv from "../assets/cv.jpg";
+import "./CV.css";
 
 class CV extends Component {
   render() {
+    const { onHide } = this.props;
     return (
-      <Container fluid>
-        <p>aquí va el cv, falta la imageeeen</p>
-        {/* <Image src={uno} className="img" /> */}
-      </Container>
+      <div>
+        {/* <p>aquí va el cv, falta la imageeeen</p> */}
+        <div className="bar">
+          <button className="close" onClick={onHide}></button>
+          <button className="close-disabled"></button>
+          <button className="close-disabled"></button>
+          <p className="name-file">CV.jpg</p>
+        </div>
+        <Image
+          src={cv}
+          className="img"
+          style={{ width: "100%", padding: "0" }}
+        />
+      </div>
     );
   }
 }
--- src/components/FormModal.js
+++ src/components/FormModal.js
@@ -1,24 +1,30 @@
 import React, { Component } from "react";
 import { Container, Row, Col, Image } from "react-bootstrap";
 // import uno from "../assets/Project1/1.jpg";
-import "./Project1.css";
+import "./FormModal.css";
 
 class FormModal extends Component {
   render() {
+    const { onHide } = this.props;
     return (
-      <Container fluid>
-        <p>
-          esto es foto o k <br /> Diseñadora UX de la Universidad Finis Terrae.
-          Estudié diseño industrial en la Universidad Diego Portales, ya que
-          llamó mi atención el desarrollo de proyectos y productos que ayudan a
-          visibilizar y solucionar problemas latentes en la sociedad. La
-          especialidad en UX Design nace a partir de la pasión por la
-          investigación, la búsqueda de nuevas metodologías y tecnologías que
-          nos ayuden a entender lo que necesitamos como sociedad, para mejorar
-          nuestra calidad de vida.
-        </p>
-        {/* <Image src={uno} className="img" /> */}
-      </Container>
+      <div style={{ height: "60vh" }}>
+        {/* <p>aquí va el cv, falta la imageeeen</p> */}
+        <div className="bar">
+          <button className="close" onClick={onHide}></button>
+          <button className="close-disabled"></button>
+          <button className="close-disabled"></button>
+          <p className="name-file">Sobre mi.txt</p>
+        </div>
+        <div className="about-me-text">
+          Diseñadora UX de la Universidad Finis Terrae. Estudié diseño
+          industrial en la Universidad Diego Portales, ya que llamó mi atención
+          el desarrollo de proyectos y productos que ayudan a visibilizar y
+          solucionar problemas latentes en la sociedad. La especialidad en UX
+          Design nace a partir de la pasión por la investigación, la búsqueda de
+          nuevas metodologías y tecnologías que nos ayuden a entender lo que
+          necesitamos como sociedad, para mejorar nuestra calidad de vida.
+        </div>
+      </div>
     );
   }
 }
